b9fd834bb37f60795ccf41d18c9215eebd355379
masud465643 <masud.bigc@gmail.com>
2026-05-10 07:21:53 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,2 +1,13 @@
 bot.py
-print("Bot setup successful")
+import telebot
+
+BOT_TOKEN = "8710393332:AAHtd20FWZwqzkk0KLbGi5Tla-dixEeS--Q"
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(func=lambda message: True)
+def reply(message):
+    bot.reply_to(message, "Message received successfully!")
+
+print("Bot is running...")
+bot.infinity_polling()
