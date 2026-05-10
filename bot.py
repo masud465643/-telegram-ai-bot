81a21a7696aee9d2290c9b8bed4c81a768ae830e
masud465643 <masud.bigc@gmail.com>
2026-05-10 07:29:22 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,16 @@
-bot.py
 import telebot
 
 BOT_TOKEN = "8710393332:AAHtd20FWZwqzkk0KLbGi5Tla-dixEeS--Q"
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Bot is working!")
+
 @bot.message_handler(func=lambda message: True)
 def reply(message):
-    bot.reply_to(message, "Message received successfully!")
+    bot.reply_to(message, "Message received successfully.")
 
 print("Bot is running...")
-bot.infinity_polling()
+bot.infinity_polling(skip_pending=True)
